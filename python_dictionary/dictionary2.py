940634e24918abbab7a3fceb937d8f2a4ee4f388
ARJIT SRIVASTAVA <arjitsrivastava@ARJITs-MacBook-Air.local>
2021-12-02 19:10:24 +0530

    Added one more dictionary program

---
+++ python_dictionary/dictionary2.py
@@ -0,0 +1,8 @@
+mydic={
+    "Pankha":"Fan",
+    "kursi":"Chair",
+    "Vastu":"Item"
+}
+print("Select a Hindi words from options ", mydic.keys())
+opt=input("Enter you word\n")
+print("Meaning of the word "+opt+" is ",mydic[opt])
