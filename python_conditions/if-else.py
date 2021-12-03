1527119d4d8b8e403f699805a499b640c2e9c1bf
ARJIT SRIVASTAVA <arjitsrivastava@ARJITs-MacBook-Air.local>
2021-12-03 16:40:38 +0530

    added if-else programs

---
+++ python_conditions/if-else.py
@@ -0,0 +1,5 @@
+a=22
+if(a<20):
+    print(a," is greater than 20")
+else:
+    print(a, "is smaller than 20")
---
+++ python_dictionary/lang_dic.py
@@ -0,0 +1,10 @@
+fav_lang={}
+friend1=input("Enter you fav language Arjit \n")
+friend2=input("Enter you fav language Anshika\n")
+friend3=input("Enter you fav language Avantika\n")
+friend4=input("Enter you fav language Ankit\n")
+fav_lang["Arjit"]=friend1
+fav_lang["Anshika"]=friend2
+fav_lang["Avantika"]=friend3
+fav_lang["Ankit"]=friend4
+print(fav_lang);
