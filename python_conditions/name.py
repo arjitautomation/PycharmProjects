ebcbb031b516e28fbe1b6a16bcd8da4a1af29117
ARJIT SRIVASTAVA <arjitsrivastava@ARJITs-MacBook-Air.local>
2021-12-03 16:45:05 +0530

    name checker

---
+++ python_conditions/name.py
@@ -0,0 +1,6 @@
+names=["Arjit","Ankit","Anshika","Avantika","Aditi"]
+name=input("Enter your name\n")
+if name in names:
+    print(name," is present in names list")
+else:
+     print(name, " is not present in names list")   
